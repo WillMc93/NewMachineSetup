0c5344820564b6c2b972f25a3a95aec05b122a8e
Will McElhenney <whm0004@auburn.edu>
2021-04-18 17:56:46 -0500

    Seems to be running

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Get up to speed
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install gcc g++ make perl curl vim
+sudo apt install gcc g++ make perl curl vim zsh r-recommended -y
 
 # Make container for downloads
 sudo mkdir /tmp/setup/
@@ -18,7 +18,8 @@ sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20N
 sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -o $tmpdir/MesloItalic.ttf
 sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -o $tmpdir/MesloIB.ttf
 
-sudo cp $/*.ttf /usr/share/fonts/truetype/
-
+sudo cp -f $tmpdir/*.ttf /usr/share/fonts/truetype/
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Get miniconda
