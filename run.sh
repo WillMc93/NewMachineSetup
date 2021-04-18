d305a70f3a4be8882d308f3cad42695e43d34d74
Will McElhenney <wmcelhe1@jhu.edu>
2021-04-18 17:44:53 -0500

    Corrections

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Get up to speed
 sudo apt update
 sudo apt full-upgrade -y
-sudo apt install gcc g++ make perl curl
+sudo apt install gcc g++ make perl curl vim
 
 # Make container for downloads
 sudo mkdir /tmp/setup/
@@ -13,13 +13,12 @@ tmpdir='/tmp/setup'
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Get fonts for powerlevel10k and then git powerlevel10k
-curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o $tmpdir/MesloReg.ttf
-curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -o $tmpdir/MesloBold.ttf
-curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -o $tmpdir/MesloItalic.ttf
-curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -o $tmpdir/MesloIB.ttf
+sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o $tmpdir/MesloReg.ttf
+sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -o $tmpdir/MesloBold.ttf
+sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -o $tmpdir/MesloItalic.ttf
+sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -o $tmpdir/MesloIB.ttf
 
-cd $tmpdir
-sudo cp *.ttf /usr/share/fonts/truetype/
+sudo cp $/*.ttf /usr/share/fonts/truetype/
 
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
