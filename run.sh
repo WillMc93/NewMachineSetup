14b6fdd31a7acafd1a9aef98dd1eb149d377e56a
Will McElhenney <wmcelhe1@jhu.edu>
2021-04-18 20:03:08 -0500

    Test 2

--- run.sh
+++ run.sh
@@ -1,54 +1,51 @@
 #!/bin/bash
 
-# Get up to speed
-sudo apt update
-sudo apt full-upgrade -y
+echo $HOME
+echo $USER
+
+# Variable Declarations
+TMPDIR='/tmp/setup'
+FONTDIR='/usr/share/fonts/truetype/'
 
 
 # Make container for downloads
-sudo mkdir /tmp/setup/
-tmpdir='/tmp/setup'
+mkdir /tmp/setup/
+
+
+# Add sublime-text keys to apt
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+
+
+# Get up to speed
+apt update
+apt full-upgrade
 
 
 # Get some essentials
-sudo apt install apt-transport-https chromium curl git gcc g++ make perl r-recommended vim zsh -y
-
-
-# Get sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-sudo apt update
-sudo apt install sublime-text -y
+apt install -y apt-transport-https chromium-browser curl git gcc g++ make perl r-recommended sublime-text vim zsh
 
 
 # Get miniconda
-sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $tmpdir/miniconda.sh
-sh $tmpdir/miniconda.sh -b -p $HOME/miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $TMPDIR/miniconda.sh
+sh $TMPDIR/miniconda.sh -b -p $HOME/miniconda
 
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init bash
 conda init zsh
 
 # install preferred packages to base
-conda install numpy scipy matplotlib ipython jupyter pandas sympy nose seaborn requests beautifulsoup4 -y
+# conda install -y numpy scipy matplotlib ipython jupyter pandas sympy nose seaborn requests beautifulsoup4
 
 
-# Get zsh and Oh-My-ZSH
+# Get Oh-My-ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Get fonts for powerlevel10k and then git powerlevel10k
-sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-sudo curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-
-sudo curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf -o $tmpdir'MesloLGS NF Regular.ttf' 
-sudo curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf -o $tmpdir'MesloLGS NF Bold.ttf' 
-sudo curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf -o $tmpdir'MesloLGS NF Italic.ttf' 
-sudo curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf -o $tmpdir'MesloLGS NF Bold Italic.ttf' 
-
-sudo cp -f $tmpdir/*.ttf /usr/share/fonts/truetype/
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf -o $FONTDIR'MesloLGS NF Regular.ttf' 
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf -o $FONTDIR'MesloLGS NF Bold.ttf' 
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf -o $FONTDIR'MesloLGS NF Italic.ttf' 
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf -o $FONTDIR'MesloLGS NF Bold Italic.ttf'
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -56,10 +53,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 
 # Cleanup
-sudo apt auto-remove -y
+apt auto-remove -y
 
 
 # Reboot into a ready environment
 echo "System will reboot in 60 secs."
 sleep 1m
-sudo reboot
+reboot
